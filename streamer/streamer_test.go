6adb5676e08a180a34e9fa6da30f0538401bab65
Thomas Hallgren <thomas@tada.se>
2019-11-09 10:39:41 +0000

    Add proper serialization of NamedType instances.

--- streamer/streamer_test.go
+++ streamer/streamer_test.go
@@ -2,6 +2,7 @@ package streamer_test
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 	"time"
 
@@ -134,4 +135,78 @@ func TestEncode_dedup_string_keys(t *testing.T) {
 	ac.Get(0).(dgo.Map).EachEntry(func(e dgo.MapEntry) { aes = append(aes, e) })
 	ac.Get(1).(dgo.Map).EachEntry(func(e dgo.MapEntry) { bes = append(bes, e) })
 	require.Same(t, aes[0].Key(), bes[0].Key())
+}
+
+type testNamed struct {
+	A string
+	B int64
+}
+
+func (v *testNamed) String() string {
+	return v.Type().(dgo.NamedType).ValueString(v)
+}
+
+func (v *testNamed) Type() dgo.Type {
+	return newtype.ExactNamed(newtype.Named(`testNamed`), v)
+}
+
+func (v *testNamed) Equals(other interface{}) bool {
+	if ov, ok := other.(*testNamed); ok {
+		return *v == *ov
+	}
+	return false
+}
+
+func (v *testNamed) HashCode() int {
+	return int(reflect.ValueOf(v).Pointer())
+}
+
+func TestEncode_namedUsingMap(t *testing.T) {
+	defer newtype.RemoveNamed(`testNamed`)
+	tp := newtype.NewNamed(`testNamed`, func(arg dgo.Value) dgo.Value {
+		sm := vf.Map(&testNamed{}).(dgo.Struct)
+		sm.PutAll(arg.(dgo.Map))
+		return sm.GoStruct().(dgo.Value)
+	}, func(value dgo.Value) dgo.Value {
+		return vf.Map(value)
+	}, reflect.TypeOf(&testNamed{}), nil)
+
+	arg := vf.Map(`A`, `hello`, `B`, 32)
+	s := streamer.New(nil, nil)
+
+	v := tp.New(arg)
+	c := streamer.NewCollector()
+	s.Stream(v, c)
+	require.Equal(t, arg.With(`__type`, `testNamed`), c.Value())
+
+	c = streamer.DataDecoder(nil, nil)
+	s.Stream(v, c)
+	require.Equal(t, v, c.Value())
+}
+
+func TestEncode_namedUsingValue(t *testing.T) {
+	defer newtype.RemoveNamed(`testNamed`)
+	tp := newtype.NewNamed(`testNamed`, func(arg dgo.Value) dgo.Value {
+		a := arg.(dgo.Array)
+		return &testNamed{A: a.Get(0).(dgo.String).GoString(), B: a.Get(1).(dgo.Integer).GoInt()}
+	}, func(value dgo.Value) dgo.Value {
+		v := value.(*testNamed)
+		return vf.Values(v.A, v.B)
+	}, reflect.TypeOf(&testNamed{}), nil)
+
+	arg := vf.Values(`hello`, 32)
+	s := streamer.New(nil, nil)
+
+	v := tp.New(arg)
+	c := streamer.NewCollector()
+	s.Stream(v, c)
+	require.Equal(t, vf.Map(`__type`, `testNamed`, `__value`, arg), c.Value())
+
+	c = streamer.DataDecoder(nil, nil)
+	s.Stream(v, c)
+	require.Equal(t, v, c.Value())
+
+	c = streamer.DataDecoder(nil, nil)
+	s.Stream(vf.Map(`__type`, `testNamed`), c)
+	require.Same(t, tp, c.Value())
 }
